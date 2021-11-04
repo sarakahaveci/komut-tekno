e5577d1ff9e15ef473b4d53cafd87461484a4fc0
yamoonz <yamooonz@gmail.com>
2021-11-04 18:13:03 +0300

    added button

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -6,21 +6,23 @@ function Header() {
       <div className="headerTitles">
         <span className="headerTitleSm">Komut Tekno</span>
       </div>
-      <div className = "head-text">
-      <img
-        className="headerImg"
-        src="https://www.ikea.com/gb/en/images/products/ektorp-3-seat-sofa-remmarn-light-grey__0924993_pe788685_s5.jpg?f=s  "
-        alt=""
-      />
-
+      <div className="head-text">
+        <img
+          className="headerImg"
+          src="https://www.ikea.com/gb/en/images/products/ektorp-3-seat-sofa-remmarn-light-grey__0924993_pe788685_s5.jpg?f=s  "
+          alt=""
+        />
       </div>
-      <div class='text-on-image'>
-      <span> Work <br/>
-       Smarter Save </span>
-      <span> Smarter </span>
-      <p>Get 10% off every month</p>
-      <button >Know More</button>
-   </div>
+      <div class="text-on-image">
+        <span>
+          {" "}
+          Work <br />
+          Smarter Save{" "}
+        </span>
+        <span> Smarter </span>
+        <p>Get 10% off every month</p>
+        <button style={{ height: "50px", width: "150px" }}>Know More</button>
+      </div>
     </div>
   );
 }
